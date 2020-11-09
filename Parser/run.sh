4a5772c74496ed8cfbd10922077a6d965c4afce8
Gabe Casciano <gabecasciano@gmail.com>
2020-11-09 16:59:27 -0500

    Parser completed

---
+++ Parser/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -classpath .:./AST TestHL
---
+++ Parser/temp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+wget http://cps710.scs.ryerson.ca/F20/A3/Handouts/HL.jjt
+wget http://cps710.scs.ryerson.ca/F20/A3/Handouts/IdentifierToken.java
+wget http://cps710.scs.ryerson.ca/F20/A3/Handouts/NumberToken.java
+wget http://cps710.scs.ryerson.ca/F20/A3/Handouts/SimpleNode.java
+wget http://cps710.scs.ryerson.ca/F20/A3/Handouts/StringToken.java
+wget http://cps710.scs.ryerson.ca/F20/A3/Handouts/TestHL.java
+wget http://cps710.scs.ryerson.ca/F20/A3/Handouts/Token.java
+wget http://cps710.scs.ryerson.ca/F20/A3/Handouts/makefile
+wget http://cps710.scs.ryerson.ca/F20/A3/Handouts/options
+wget http://cps710.scs.ryerson.ca/F20/A3/Handouts/runtestsa
+wget http://cps710.scs.ryerson.ca/F20/A3/Handouts/runtestsb
+wget http://cps710.scs.ryerson.ca/F20/A3/Handouts/runtestsc
+wget http://cps710.scs.ryerson.ca/F20/A3/Handouts/ta
+wget http://cps710.scs.ryerson.ca/F20/A3/Handouts/tb
+wget http://cps710.scs.ryerson.ca/F20/A3/Handouts/tc
